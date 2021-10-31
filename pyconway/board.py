8a39d84e98aaec81a8b5a2738aefc92b64c0dd5f
jelambrar96 <jelambrar@gmail.com>
2021-10-31 11:49:03 -0500

    add init with board

--- pyconway/board.py
+++ pyconway/board.py
@@ -12,17 +12,27 @@ class ConwayBoard:
 
     _kernel = np.array([1, 1, 1], [1, 0, 1], [1, 1, 1])
 
-    def __init__(self, size):
-        self.size = size
-        self.state  = np.zeros(size, dtype=bool)
+    def __init__(self, size=None, board=None):
+        assert(size is not None or board is not None, 
+                "Both arguments have not be None at the same time")
+        if size is None and board is not None:
+            self.state = board 
+            self.size = board.shape
+        elif size is not None and board is None:
+            self.size = size
+            self.state  = np.zeros(size, dtype=bool)
+        elif size is not None and board is not None:
+            assert(size == board.shape, "Size and board does not Match")
+            self.state = board 
+            self.size = board.shape
 
-    def clear(self):
-        self.state = np.zeros(self.size, dtype=bool)
+    def getState(self):
+        return self.state
 
     def __next(self):
         conv_result = convolve2d(self.state, ConwayBoard._kernel, boundary='fill', fillvalue=0)
         temp = np.where(conv_result == 3, 1, 0)
-        temp[np.where(self.state == 0 & conv_result == 2)]
+        temp[np.where(self.state == 0 & conv_result == 2)] = 1
         self.state = temp 
 
     def next(self, iterations = 1):
