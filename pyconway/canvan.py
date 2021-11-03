084ca66a1d2eea317376bb60820ab8ffcc7fc0b6
jelambrar96 <jelambrar@gmail.com>
2021-11-03 02:28:26 -0500

    canvan is working

--- pyconway/canvan.py
+++ pyconway/canvan.py
@@ -1,16 +1,31 @@
+import numpy as np
 
 # from .board import ConwayBoard
 
 class ConwayCanvan:
 
-    def __init__(self, state, scale=1):
-        pass
+    def __init__(self, scale=1):
+        self._bgd_color
+        self._fgd_color
+        self._scale = scale
 
-    def setBackgroud(self, color=None, bgd_matrix=None):
-        pass
+    def getBackground(self):
+        return self._bgd_color
+    
+    def getForeground(self):
+        return self._fgd_color
 
-    def setForeground(self, color=None, bgd_matrix=None):
-        pass
+    def getScale(self):
+        return self._scale
+
+    def setBackgroud(self, color):
+        self._bgd_color = color
+
+    def setForeground(self, color):
+        self._fgd_color = color
 
     def apply(self, conway_state):
-        pass
+        temp_state = conway_state.repeat(self._scale, axis=0).repeat(self._scale, axis=1)
+        # dimentions must be match!!!!!
+        # use a broadcast if any color is a number
+        return np.where(temp_state, self._fgd_color, self._bgd_color)
