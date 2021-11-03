9df1f33646c597c12f704ade052cbdfc7b195e5a
jelambrar96 <jelambrar@gmail.com>
2021-11-03 02:05:23 -0500

    add randomc

--- pyconway/randomc.py
+++ pyconway/randomc.py
@@ -2,4 +2,4 @@ import numpy as np
 
 def createRandomBoard(size, threshold=0.5):
     board = np.random.random(size)
-    return np.where(board > threshold, 0, 1)
+    return np.where(board > threshold, 0, 1).astype(np.uint8)
