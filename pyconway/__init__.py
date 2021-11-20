4738042d87da9039c9fa57e230eb1a8e415dbee2
jelambrar96 <jelambrar@gmail.com>
2021-11-19 23:23:42 -0500

    pyconway/__init__.py

--- pyconway/__init__.py
+++ pyconway/__init__.py
@@ -4,8 +4,8 @@ from .board import ConwayBoard, ConwayBoard3C, ConwayBoard3D
 from .canvan import ConwayCanvan
 from .simulator import Simulator
 
-from .generator import GifGenerator
-from .radomc import createRandomState
+from .generator import GifGenerator, MP4VideoGenerator
+from .randomc import createRandomState
 
 __author__ = "jelambrar96 <jelambrar@gmail.com>"
 __version__ = pkg_resources.get_distribution("pyconway").version
