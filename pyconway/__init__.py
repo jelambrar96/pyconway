2c09570261277bb03a33a631411e1df03b9b1ac0
jelambrar96 <jelambrar@gmail.com>
2021-11-19 23:25:12 -0500

    pyconway/__init__.py

--- pyconway/__init__.py
+++ pyconway/__init__.py
@@ -4,7 +4,7 @@ from .board import ConwayBoard, ConwayBoard3C, ConwayBoard3D
 from .canvan import ConwayCanvan
 from .simulator import Simulator
 
-from .generator import GifGenerator, MP4VideoGenerator
+from .generator import GifGenerator
 from .randomc import createRandomState
 
 __author__ = "jelambrar96 <jelambrar@gmail.com>"
