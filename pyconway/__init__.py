07dc360c3a517022dbbea40f6495ef4b3195da3c
jelambrar96 <jelambrar@gmail.com>
2021-11-19 23:27:08 -0500

    add mp4 video generator to init.py

--- pyconway/__init__.py
+++ pyconway/__init__.py
@@ -4,7 +4,7 @@ from .board import ConwayBoard, ConwayBoard3C, ConwayBoard3D
 from .canvan import ConwayCanvan
 from .simulator import Simulator
 
-from .generator import GifGenerator
+from .generator import GifGenerator, MP4VideoGenerator
 from .randomc import createRandomState
 
 __author__ = "jelambrar96 <jelambrar@gmail.com>"
